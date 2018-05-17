4f5a0f768233a123ccaeae9a061c414e63741341
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-05-17 15:03:06 -0300

    Menu implementado

--- main.c
+++ main.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
 
+void salida(unsigned char x) {
+
+};
+
+void autoFantastico() {
+
+};
+
+void choque() {
+
+};
+
+void carrera() {
+
+};
+
+void opcion4() {
+
+};
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char entrada[1];
+    while (1 == 1) {
+        printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Carrera\n 4)Opcion4\n 5)Salir\n");
+        scanf("%c", entrada);
+        //printf("%c", entrada[0]);
+        switch (entrada[0]) {
+            case '1':
+                autoFantastico();
+                break;
+
+            case '2':
+                choque();
+                break;
+
+            case '3':
+                carrera();
+                break;
+
+            case '4':
+                opcion4();
+                break;
+
+            case '5':
+                return 1;
+
+            default:
+                printf("\nEntrada invalida\nSeleccione nuevamente una opción\n");
+                break;
+        }
+    }
 }
