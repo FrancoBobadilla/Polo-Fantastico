a8e4bc06d9cc0e14ffa67760374fae2d93d73d32
NNelo <nelogino@gmail.com>
2018-05-22 16:06:54 -0300

    Se implementa ingreso por password

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+char clave[5] = {'c','l','a','v','e'};
+
+int login(){
+	printf("Ingrese su contrasena de 5 digitos\n");
+	int i;
+	char c[5];
+	int cantIntentos = 3;
+	
+	while(0 != cantIntentos){
+		cantIntentos--;
+		i = 5;
+		do{
+			i--;
+			c[4 - i] = getch();
+			printf("*");
+		
+		} while(i != 0);
+	
+		if(0 == strcmp(clave, c)){
+	 		printf("\nBienvenido al sistema");
+	 		return 1;
+		}		
+		else{
+			printf("\nEntrada incorrecta. Intentos restantes: ");
+			printf("%d", cantIntentos); 
+			printf("\n");
+		}		
+	}
+	return 0;
+}
+
 void salida(unsigned char x) {
     unsigned char i = 8;
     do {
@@ -30,33 +61,35 @@ void opcion4() {
 
 int main() {
     char entrada[1];
-    do {
-        printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Carrera\n 4)Opcion4\n 5)Salir\n");
-        scanf("%c", entrada);
-        //printf("%c", entrada[0]);
-        switch (entrada[0]) {
-            case '1':
-                autoFantastico();
-                break;
+    if(login()){
+    	do {
+       		printf("\nMenu principal\n 1)Auto fantastico\n 2)Choque\n 3)Carrera\n 4)Opcion4\n 5)Salir\n");
+        	scanf("%c", entrada);
+        	//printf("%c", entrada[0]);
+        	switch (entrada[0]) {
+            	case '1':
+                	autoFantastico();
+                	break;
 
-            case '2':
-                choque();
-                break;
+            	case '2':
+                	choque();
+                	break;
 
-            case '3':
-                carrera();
-                break;
+            	case '3':
+                	carrera();
+                	break;
 
-            case '4':
-                opcion4();
-                break;
+            	case '4':
+                	opcion4();
+                	break;
 
-            case '5':
-                return 1;
+            	case '5':
+                	return 1;
 
-            default:
-                printf("\nEntrada invalida\nSeleccione nuevamente una opción\n");
-                break;
-        }
-    } while (1);
+            	default:
+                	printf("\nEntrada invalida\nSeleccione nuevamente una opción\n");
+                	break;
+        	}
+    	} while (1);
+	}
 }
