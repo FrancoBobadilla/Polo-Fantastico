72e96892c1f12b1c180d63724a07bbe04a0eddcf
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-05-31 15:10:18 -0300

    se hizo algo

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
 
-unsigned long int TIEMPO = 2000;
-const char clave[] = "clave";
+unsigned long int tiempoAutoFantastico = 1000;
+unsigned long int tiempoChoque = 1000;
+unsigned long int tiempoCarrera = 1000;
+unsigned long int tiempoOpcion4 = 1000;
+const char clave[] = {'c','l','a','v','e'};
 unsigned char tablaChoque[] = {0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81};
-
-char retardo(unsigned long int);
+	
+char retardo(unsigned long int *);
 
 void clearScreen();
 
@@ -21,7 +24,7 @@ void carrera();
 
 void opcion4();
 
-void main() {
+void main() {	
     char entrada[1];
     if (login()) {
         do {
@@ -57,25 +60,7 @@ void main() {
     }
 }
 
-char retardo(unsigned long int a) {
-    while (a) {
-        a--;
-        //if (kbhit()){
-        	//char c = fgetc(stdin);
-        	//char c= getch();
-        	if (getchar() == 'x'){
-        		TIEMPO >>= 1;
-			}else{
-				if (getchar() == 'y'){
-					TIEMPO <<= 1;
-				}else{
-					return 1;
-				}
-		//	}
-		}
-    }
-    return 0;
-}
+
 
 void clearScreen() {
    system("cls");
@@ -83,7 +68,7 @@ void clearScreen() {
 }
 
 int login() {
-	return 1;
+	return 1; 										/**//**//**//**//**//**//**//**//**/
     printf("Ingrese su contrasena de 5 digitos\n");
     int i;
     char c[5];
@@ -100,7 +85,7 @@ int login() {
         if (0 == strcmp(clave, c)) {
             clearScreen();
             printf("Bienvenido al sistema");
-            retardo(TIEMPO * 3);
+            retardo(& tiempoAutoFantastico);
             return 1;
         } else {
             printf("\nEntrada incorrecta. Intentos restantes: ");
@@ -124,6 +109,30 @@ void salida(unsigned char x) {
     } while (i != 0);
 }
 
+char retardo(unsigned long int *a) {
+	unsigned long int b = * a;
+	while (b) {
+        b--;
+        
+        if(kbhit()){
+        	char c = getch();
+        	if('x' == c){
+        		b = b * 2;
+        		(*a) = (*a) * 2;
+			}else{
+				if('y' == c){
+					b = b / 2;
+					(*a) = (*a) / 2;
+				}else{
+					return 1;
+				}
+			}
+		}
+		
+    }
+    return 0;
+}
+
 void autoFantastico() {
     unsigned char i;
     clearScreen();
@@ -131,24 +140,25 @@ void autoFantastico() {
     while (1) {
         for (i = 128; i > 0; i >>= 1) {
             salida(i);
-            if (retardo(TIEMPO))
+            if (retardo(& tiempoAutoFantastico))
                 return;
         }
         for(i= 2; i < 128; i <<= 1){
         	salida(i);
-            if (retardo(TIEMPO))
+            if (retardo(& tiempoAutoFantastico))
                 return;
 		}
     }
 }
 
 void choque() {
+	char i;	//notar que es char y no unsigned char
 	clearScreen();
-	char i;
+	printf("Choque!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
     while(1){
     	 for (i = 6; i >= 0; i--) {
             salida(tablaChoque[i]);
-            if (retardo(TIEMPO))
+            if (retardo(& tiempoChoque))
                 return;
         }
 	}
