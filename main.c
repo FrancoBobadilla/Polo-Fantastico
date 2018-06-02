130936811c3149b73247604e82da7f01195e101b
NNelo <nelogino@gmail.com>
2018-06-02 14:14:43 -0300

    Se eliminan variables globales (problema rapidez)

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
-
-unsigned long int tiempoAutoFantastico = 1000;
-unsigned long int tiempoChoque = 1000;
-unsigned long int tiempoCarrera = 1000;
-unsigned long int tiempoOpcion4 = 1000;
-const char clave[] = {'c','l','a','v','e'};
-unsigned char tablaChoque[] = {0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81};
+#include <stdlib.h>		//para system()
 	
 char retardo(unsigned long int *);
 
@@ -16,37 +10,41 @@ int login();
 
 void salida(unsigned char);
 
-void autoFantastico();
+void autoFantastico(unsigned long int *);
 
-void choque();
+void choque(unsigned long int *);
 
-void carrera();
+void opcion3(unsigned long int *);
 
-void opcion4();
+void opcion4(unsigned long int *);
 
 void main() {	
+	unsigned long int tiempoAutoFantastico = 1000;
+	unsigned long int tiempoChoque = 1000;
+	unsigned long int tiempoOpcion3 = 1000;
+	unsigned long int tiempoOpcion4 = 1000;
+
     char entrada[1];
     if (login()) {
         do {
             clearScreen();
-            printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Carrera\n 4)Opcion4\n 5)Salir\n");
+            printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Opcion3\n 4)Opcion4\n 5)Salir\n");
             scanf("%c", entrada);
-            //printf("%c", entrada[0]);
             switch (entrada[0]) {
                 case '1':
-                    autoFantastico();
+                    autoFantastico(&tiempoAutoFantastico);
                     break;
 
                 case '2':
-                    choque();
+                    choque(&tiempoChoque);
                     break;
 
                 case '3':
-                    carrera();
+                    opcion3(&tiempoOpcion3);
                     break;
 
                 case '4':
-                    opcion4();
+                    opcion4(&tiempoOpcion4);
                     break;
 
                 case '5':
@@ -60,15 +58,15 @@ void main() {
     }
 }
 
-
-
 void clearScreen() {
    system("cls");
    //clrscr();
 }
 
 int login() {
-	return 1; 										/**//**//**//**//**//**//**//**//**/
+	const char clave[] = {'c','l','a','v','e'};
+	unsigned long int tiempoBienvenida = 100000;
+	//return 1; 										/**//**//**//**//**//**//**//**//**/
     printf("Ingrese su contrasena de 5 digitos\n");
     int i;
     char c[5];
@@ -84,8 +82,13 @@ int login() {
         } while (i != 0);
         if (0 == strcmp(clave, c)) {
             clearScreen();
-            printf("Bienvenido al sistema");
-            retardo(& tiempoAutoFantastico);
+            printf(" ----------------------- ");
+            printf("\n");
+            printf("| Bienvenido al sistema |");
+            printf("\n");
+            printf(" ----------------------- ");
+            printf("\n");
+            retardo(&tiempoBienvenida);
             return 1;
         } else {
             printf("\nEntrada incorrecta. Intentos restantes: ");
@@ -116,11 +119,11 @@ char retardo(unsigned long int *a) {
         
         if(kbhit()){
         	char c = getch();
-        	if('x' == c){
+        	if('z' == c){		//disminuye rapidez
         		b = b * 2;
         		(*a) = (*a) * 2;
 			}else{
-				if('y' == c){
+				if('x' == c){	//aumenta rapidez
 					b = b / 2;
 					(*a) = (*a) / 2;
 				}else{
@@ -128,46 +131,46 @@ char retardo(unsigned long int *a) {
 				}
 			}
 		}
-		
     }
     return 0;
 }
 
-void autoFantastico() {
+void autoFantastico(unsigned long int *tiempoAutoFantastico) {
     unsigned char i;
     clearScreen();
     printf("Auto fantastico!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
     while (1) {
         for (i = 128; i > 0; i >>= 1) {
             salida(i);
-            if (retardo(& tiempoAutoFantastico))
+            if (retardo(tiempoAutoFantastico))
                 return;
         }
         for(i= 2; i < 128; i <<= 1){
         	salida(i);
-            if (retardo(& tiempoAutoFantastico))
+            if (retardo(tiempoAutoFantastico))
                 return;
 		}
     }
 }
 
-void choque() {
+void choque(unsigned long int *tiempoChoque) {
 	char i;	//notar que es char y no unsigned char
+	unsigned char tablaChoque[] = {0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81};
 	clearScreen();
 	printf("Choque!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
     while(1){
     	 for (i = 6; i >= 0; i--) {
             salida(tablaChoque[i]);
-            if (retardo(& tiempoChoque))
+            if (retardo(tiempoChoque))
                 return;
         }
 	}
 }
 
-void carrera() {
+void opcion3(unsigned long int *tiempoOpcion3) {
 
 }
 
-void opcion4() {
+void opcion4(unsigned long int *tiempoOpcion4) {
 
 }
