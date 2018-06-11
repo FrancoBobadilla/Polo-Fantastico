3d1022ee81ef38bd51abcde0b2a5585edaf34ca0
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-06-10 21:03:54 -0300

    se implemento 4ta secuencia (piedra estanque) y se muestra el retardo actual

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <conio.h>
 #include <stdlib.h>        //para system()
 
-#define TMAX 1700
-#define TMED 1000
-#define TMIN 200
+#define TMAX 1000
+#define TMED 500
+#define TMIN 100
+#define INTERVALO 100
 
 char retardo(unsigned long int *);
 
@@ -12,15 +13,15 @@ void clearScreen();
 
 int login();
 
-void salida(unsigned char);
+void salida(unsigned char, unsigned long int);
 
-void autoFantastico(unsigned long int *);
+void autoFantastico(unsigned long int *);    //algoritmo
 
-void choque(unsigned long int *);
+void choque(unsigned long int *);    //tabla
 
-void policia(unsigned long int *);
+void policia(unsigned long int *);    //algoritmo
 
-void opcion4(unsigned long int *);
+void piedraEstanque(unsigned long int *);    //tabla
 
 char arraycmp(const char *, const char *, unsigned int);
 
@@ -28,35 +29,38 @@ void main() {
     unsigned long int tiempoAutoFantastico = TMED;
     unsigned long int tiempoChoque = TMED;
     unsigned long int tiempoPolicia = TMED;
-    unsigned long int tiempoOpcion4 = TMED;
+    unsigned long int tiempoPiedraEstanque = TMED;
+    unsigned long int tiempoError = TMAX;
     char entrada;
     if (login()) {
         do {
             clearScreen();
-            printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Policia\n 4)Opcion4\n 5)Salir\n");
+            printf("Menu principal\n 1)Auto fantastico\n 2)Choque\n 3)Policia\n 4)Piedra cayendo sobre estanque\n 5)Salir\n");
             entrada = getch();
             switch (entrada) {
                 case '1':
-                    autoFantastico(&tiempoAutoFantastico);
+                    autoFantastico(&tiempoAutoFantastico);    //algoritmo
                     break;
 
                 case '2':
-                    choque(&tiempoChoque);
+                    choque(&tiempoChoque);    //tabla
                     break;
 
                 case '3':
-                    policia(&tiempoPolicia);
+                    policia(&tiempoPolicia);    //algoritmo
                     break;
 
                 case '4':
-                    opcion4(&tiempoOpcion4);
+                    piedraEstanque(&tiempoPiedraEstanque);    //tabla
                     break;
 
                 case '5':
                     break;
 
                 default:
-                    printf("\nEntrada invalida\nSeleccione nuevamente una opci�n\n");
+                    clearScreen();
+                    printf("\n\n     ENTRADA INVALIDA");
+                    retardo(&tiempoError);
                     break;
             }
         } while (entrada != '5');
@@ -65,11 +69,9 @@ void main() {
 
 void clearScreen() {
     system("cls");
-    //clrscr();
 }
 
 int login() {
-    //return 1;
     const char clave[] = {'c', 'l', 'a', 'v', 'e'};
     unsigned long int tiempoBienvenida = TMAX;
     printf("Ingrese su contrasena de 5 digitos\n");
@@ -104,44 +106,52 @@ int login() {
     return 0;
 }
 
-void salida(unsigned char x) {
+void salida(unsigned char x, unsigned long int tiempo) {
     unsigned char i = 8;
     printf("\r");
     do {
         i--;
         if ((x & (1 << i)) != 0) {
-            printf("*");
+            printf("*"); //en caso de implementarse sobre LEDs, deberia cambiarse por la funcion de salida adecuada
         } else {
-            printf("_");
+            printf("_"); //en caso de implementarse sobre LEDs, deberia cambiarse por la funcion de salida adecuada
         }
     } while (i != 0);
+
+    //impreson de velocidad actual
+    printf("   Retardo minimo: ");
+    printf("%d", TMIN);
+    printf("   Retardo actual: ");
+    printf("%d", tiempo);
+    printf("   Retardo maximo: ");
+    printf("%d", TMAX);
+    printf("   ");
 }
 
 char retardo(unsigned long int *a) {
     unsigned long int b = *a;
     int c;
-    printf("    Velocidad:");
-    printf("%d", *a);
     while (b--) {
         if (kbhit()) {
             c = getch();
-            if (c == 224 || c == 0) {    //224: teclas de las fechas de navegacion principales, 0: funciones especiales, flechas de navegacion secundarias (del teclado numerico, cuadno no esta activado el bloc num) incluidas
+            if (c == 224 || c ==
+                            0) {    //224: teclas de las fechas de navegacion principales, 0: funciones especiales, flechas de navegacion secundarias (del teclado numerico, cuadno no esta activado el bloc num) incluidas
                 if (kbhit())
                     c = getch();
                 else
-                    return 0;    //en caso de que el usuario ingrese (no se c�mo) el char "0"
+                    return 0;    //en caso de que el usuario ingrese (no se como) el char 0 (NULL)
                 switch (c) {
                     case 80:
                         if ((*a) < TMAX) {
                             b = b / (*a);
-                            (*a) = (*a) + 100;
+                            (*a) = (*a) + INTERVALO;
                             b = b * (*a);
                         }
                         break;
                     case 72:
                         if ((*a) > TMIN) {
                             b = b / (*a);
-                            (*a) = (*a) - 100;
+                            (*a) = (*a) - INTERVALO;
                             b = b * (*a);
                         }
                         break;
@@ -150,7 +160,7 @@ char retardo(unsigned long int *a) {
                 }
             } else {
                 if (kbhit())
-                    getch();    //en caso de que el usuario entre un doble char, y que el primero no sea "0" ni "224"
+                    getch();    //en caso de que el usuario ingrese un doble char, y que el primero no sea "0" ni "224"
                 return 1;
             }
 
@@ -159,22 +169,22 @@ char retardo(unsigned long int *a) {
     return 0;
 }
 
-void autoFantastico(unsigned long int *direccionTiempo) {
+void autoFantastico(unsigned long int *direccionTiempo) {    //algoritmo
     unsigned char i;
     clearScreen();
     printf("Auto fantastico!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
     while (1) {
         for (i = 128; i > 0; i >>= 1) {
-            salida(i);
+            salida(i, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
         for (i = 2; i < 128; i <<= 1) {
-            salida(i);
+            salida(i, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
@@ -188,99 +198,114 @@ void choque(unsigned long int *direccionTiempo) {
     printf("Choque!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
     while (1) {
         for (i = 6; i >= 0; i--) {
-            salida(tablaChoque[i]);
+            salida(tablaChoque[i], *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
     }
 }
 
-void policia(unsigned long int *direccionTiempo) {
+void policia(unsigned long int *direccionTiempo) {    //algoritmo
     char i;
-    unsigned char tablaPolicia[] = {
+    clearScreen();
+    printf("Policia!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
+
+    /*
+	unsigned char tablaPolicia[] = {
             0x00, 0x00, 0x00, 0x0F, 0x0F, 0x0F,
             0x00, 0x00, 0x00, 0xF0, 0xF0, 0xF0,
             0x00, 0x0F, 0x00, 0x0F, 0x00, 0x0F,
             0x00, 0xF0, 0x00, 0xF0, 0x00, 0xF0
     };
-
-    clearScreen();
-    printf("Policia!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
-    // while (1) {
-    //     for (i = 23; i >= 0; i--) {
-    //         salida(tablaPolicia[i]);
-    //         if (retardo(tiempoPolicia))
-    //             return;
-    //     }
-    // }
+    */
 
     while (1) {
+
         for (i = 3; i > 0; i--) {
-            salida(0xF0);
+            salida(0xF0, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
-            salida(0x00);
+            salida(0x00, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
+
         for (i = 3; i > 0; i--) {
-            salida(0x0F);
+            salida(0x0F, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
-            salida(0x00);
+            salida(0x00, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
-                return;
-            }
-        }
-        for (i = 3; i > 0; i--) {
-            salida(0xF0);
-            if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
+
         for (i = 3; i > 0; i--) {
-            salida(0x00);
+            salida(0xF0, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
+
         for (i = 3; i > 0; i--) {
-            salida(0x0F);
+            salida(0x00, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
                 return;
             }
         }
+
         for (i = 3; i > 0; i--) {
-            salida(0x00);
+            salida(0x0F, *direccionTiempo);
             if (retardo(direccionTiempo)) {
-                salida(0);
+                salida(0, *direccionTiempo);
+                return;
+            }
+        }
+
+        for (i = 3; i > 0; i--) {
+            salida(0x00, *direccionTiempo);
+            if (retardo(direccionTiempo)) {
+                salida(0, *direccionTiempo);
+                return;
+            }
+        }
+
+    }
+}
+
+void piedraEstanque(unsigned long int *direccionTiempo) {
+    char i;    //notar que es char y no unsigned char
+    unsigned char tablaPiedraEstanque[] = {0x00, 0x00, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24, 0x42, 0x42, 0x42, 0x81, 0x81,
+                                           0x42, 0x24, 0x18, 0x10, 0x20, 0x40, 0x80};
+    clearScreen();
+    printf("Piedra cayendo sobre estanque!!!\n Para aumentar la velocidad presione la flecha para arriba\n Para disminuir la velocidad presione la flecha para abajo\n Para volver al menu presione cualquier otra tecla\n\n");
+    while (1) {
+        for (i = 20; i >= 0; i--) {
+            salida(tablaPiedraEstanque[i], *direccionTiempo);
+            if (retardo(direccionTiempo)) {
+                salida(0, *direccionTiempo);
                 return;
             }
         }
     }
 }
 
-void opcion4(unsigned long int *tiempoOpcion4) {
-
-}
-
-char arraycmp(const char *a, const char *b, unsigned int tamanio) {
+char arraycmp(const char *a, const char *b, unsigned int tamanio) {    //usada unicamente en funcion login()
     int i;
-    for (i = 0; i < tamanio; ++i) {
+    for (i = 0; i < tamanio; i++) {
         if (a[i] != b[i])
-            return 1;
+            return 1;    //retorna 1 si no son iguales
     }
-    return 0;
+    return 0;    //retorna 0 si son iguales
 }
